f0e27a0eaa4d87c13174d348f37f0dd667ef477a
TimBreak <tim.kuz.12@yandex.ru>
2026-06-11 21:44:15 +0300

    i do not know what is it

--- Compiler/obj/Debug/net10.0/Compiler.AssemblyInfo.cs
+++ Compiler/obj/Debug/net10.0/Compiler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Compiler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f7a7f5c878b22eb7699fa1320b2d47bace2826d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Compiler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Compiler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
